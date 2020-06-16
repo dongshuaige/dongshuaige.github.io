f78a0d67b1815b1f2a657789a80e4bc58068535c
fy <1465158178@qq.com>
2020-06-16 20:49:11 +0800

    Site updated: 2020-06-16 20:49:08

---
+++ js/VolantisTags.js
@@ -0,0 +1,34 @@
+// 去除banner
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+    full_page[0].style.background = "transparent";
+}
+
+// 手机侧边栏默认不展开
+var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+    "menus_item_child"
+);
+var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+    menus_item_child[i].style.display = "none";
+    menus_expand[i].className += " menus-closed";
+}
+
+
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = 'ヽ(●-`Д´-)ノ你要走嘛我好伤心！';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '(Ő∀Ő3)ノ哇喔！欢迎！' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
